40a0eefc9bd32e7a63bbf20d0c26daa5c7733b26
Mark Savushkin <mark.savushkin@bimeister.com>
2023-02-05 19:50:13 +0300

    ТЗ №16

--- gateway/src/main/java/ru/practicum/shareit/booking/validator/BookingDateConstraint.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/validator/BookingDateConstraint.java
@@ -6,10 +6,12 @@ import java.lang.annotation.*;
 
 @Documented
 @Constraint(validatedBy = BookingDatesValidator.class)
-@Target( { ElementType.TYPE })
+@Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface BookingDateConstraint {
     String message() default "Дата начала бронирования не может быть позже даты окончания!";
+
     Class<?>[] groups() default {};
+
     Class<? extends Payload>[] payload() default {};
 }
